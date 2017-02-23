4772b6b53716f015e9c7cac405b9f8f3fff5a241
Adarsh Nair <adarshnair01@gmail.com>
2017-02-23 12:18:47 +0530

    Removed Connection String

--- BootCampWebApi/BootCampWebApi/Constants.cs
+++ BootCampWebApi/BootCampWebApi/Constants.cs
@@ -7,7 +7,7 @@ namespace BootCampWebApi
 {
     public class Constants
     {
-        public static string ConnectionString = "Data Source=DESKTOP-483S0CP\\SQLEXPRESS;Initial Catalog=Ext_Bootcamp;Integrated Security=True";
+        public static string ConnectionString = ConnectionString;
         public static string ProductSelectStatement = "SELECT * FROM [dbo].[udf_Top10ProductSales] ('{0}')";
         public static string CitySelectStatement = "Select [City], [Total Sales] FROM [dbo].[vw_TopTenSalesCities];";
     }
